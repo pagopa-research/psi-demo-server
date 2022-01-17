424969b0a31a28940ea710a54150829b8d004d19
Stefano Conoci <conoci@lockless.it>
2022-01-17 12:04:00 +0100

    Added basic spring project and gitignore

---
+++ src/main/java/it/lockless/psidemoserver/PsiDemoServerApplication.java
@@ -0,0 +1,13 @@
+package it.lockless.psidemoserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PsiDemoServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PsiDemoServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/it/lockless/psidemoserver/PsiDemoServerApplicationTests.java
@@ -0,0 +1,13 @@
+package it.lockless.psidemoserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PsiDemoServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
